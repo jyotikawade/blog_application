ff8fd081a4eb5f285969784f0ae1de9ee2e4caf3
JyotiKawade <jkawade2000@gmail.com>
2025-07-20 15:10:22 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+# Create your models here.
+# this line defines our model (it is an object). Always start a class name with an uppercase letter
+
+
+class Post(models.Model):
+    auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
