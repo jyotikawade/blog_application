a5039be06707bb26f5a6313210aaaadafc959d31
JyotiKawade <jkawade2000@gmail.com>
2025-07-21 23:43:44 +0530

    changes similar to django girls app

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(blank=True, null=True)
